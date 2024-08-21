da581ae35cee8e2687112e69dc87c1389c5c2ff3
Faraz Ibrar <farazibrar9697@gmail.com>
2024-08-21 09:48:59 +0500

    Adding more

---
+++ 285-react-styling-practice-completed/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Heading from "./heading";
+
+function App(){
+    return(
+        <div>
+            <Heading />
+        </div>
+    )
+}
+
+export default App
